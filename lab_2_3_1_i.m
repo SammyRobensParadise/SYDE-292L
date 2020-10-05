f8dd59fa9896e3e09b29c4ac3a72b6e9c85e4c9b
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-10-05 16:48:09 -0400

    completed

--- lab_2_3_1_i.m
+++ lab_2_3_1_i.m
@@ -1,2 +1,19 @@
 % setup range
-w=40*-1:100;
+w=-42:200;
+
+%variables
+
+R2=500;
+R3=10E6;
+C4=47E-6;
+
+% equation
+Av=R3./((w*C4*R3*R2)+R3+R2);
+AvEq=20*log(Av);
+
+% plot
+plot(w,AvEq);
+title('Gain/Frequency Fig 2.2');
+ylabel('A(w)');
+xlabel('w');
+legend({'20log(Vb(w)/Va(w))'},'Location','southwest')
