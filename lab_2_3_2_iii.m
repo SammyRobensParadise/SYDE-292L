5b3d8cdd69386a4c862dec77c2d36c8120726a0a
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-10-05 22:41:41 -0400

    created section for 3.2-iii

---
+++ lab_2_3_2_iii.m
@@ -0,0 +1,25 @@
+% get array of frequencies
+frequencies = [100 200 300 400 500 600 700 800 900 1000 2000 4000 10000];
+
+% setup default values for equation
+R=4700;
+C=0.033e-6;
+Vin=10;
+
+% loop through and calculate Vout then display
+for f=1:13
+    % get expression
+    
+    % calculating output voltage
+    Xc=((2*pi*frequencies(f)*C));
+    denom=sqrt((1)+(Xc^2));
+    ratio=Xc/denom;
+    disp(frequencies(f));
+    Vout=Vin*ratio;
+    
+    % calculating gain
+    
+    % displaying answers
+    disp('Output Voltage V: ');
+    disp(Vout);
+end
