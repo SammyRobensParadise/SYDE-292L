ad888dbe3bda441b2faa4dc23cc48fc46e1f18d6
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-11-17 17:21:15 -0500

    created lab 5.3.3

---
+++ Lab_5_3_3.m
@@ -0,0 +1,25 @@
+% Sammy Robens-Paradise
+% get array of frequencies
+freq = [100 200 300 400 500 600 700 800 900 1000 2000 4000 100000];
+
+% setup default values for equation
+Q=0.707;
+FreqC=736;
+Omega=(2*pi*FreqC);
+Omega_Squared=(Omega)^2;
+disp(Omega)
+% loop through and calculate Vout then display
+for f=1:13
+    current_freq=freq(f);
+    current_Omega =(2*pi*current_freq);
+    current_Omega_Squared=(current_Omega)^2;
+    voltage_gain=(Omega_Squared)/((current_Omega_Squared)+(Omega/Q)+Omega_Squared);
+    dBGain=20*log10(voltage_gain);
+    disp('frequency Hz')
+    disp(current_freq)
+    disp('voltage Gain')
+    disp(abs(voltage_gain));
+    disp('dB gain')
+    disp(dBGain)
+    disp('________________________')
+end
