871f8001f7a6d7059fdd0792d295fe13b1f4d8be
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-11-18 21:59:57 -0500

    fixed Lab5.3.5

--- Lab_5_3_5.m
+++ Lab_5_3_5.m
@@ -8,15 +8,15 @@ Rd=1e6;
 R=10e3;
 C=15e-9;
 j=sqrt(-1);
-neg_1_over_Rg_C=((-1)/(Rg*C));
+one_over_Rg_C=((1)/(Rg*C));
 one_over_R_C_Sqrd=((1)/((R*C)^2));
+Rd_C=(Rd*C);
 % loop through and calculate Vout then display
 for f=1:6
     current_freq=freq(f);
-    current_Omega =(j*2*pi*current_freq);
-    current_Omega_Squared=(current_Omega)^2;
-    disp(neg_1_over_Rg_C*2*pi*current_freq)
-    voltage_gain=abs((neg_1_over_Rg_C*2*pi*current_freq)/(current_Omega_Squared+(current_Omega_Squared/(Rd*C))+one_over_R_C_Sqrd));
+    s =(j*2*pi*current_freq);
+    s_qrd=(s)^2;
+    voltage_gain=abs(((-1)*one_over_Rg_C*s)/(s_qrd+(s/Rd_C)+one_over_R_C_Sqrd));
     dBGain=20*log10(voltage_gain);
     disp('frequency Hz')
     disp(current_freq)
